6393388387a1db25007ac705c7e2bbf4c2b26c6a
neha nikam <nikam.neha93@gmail.com>
2019-01-01 07:51:14 +0530

    making changes for az-linux2

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 yum update -y 
 
 # install jenkins 
-
+sudo yum remove java -y
 sudo yum install java-1.8.0-openjdk -y
 sudo yum install -y wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
@@ -13,6 +13,10 @@ sudo service jenkins start
 sudo chkconfig jenkins on
 
 # install ansible
-sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum update -y
-sudo yum install ansible -y
+#sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm #centos6.8
+# sudo yum update -y
+# sudo yum install ansible -y
+
+#install ansible az linux2
+sudo yum-config-manager --enable epel #amaz-linux
+yum install ansible
